b55449cb550d4db965322e6218c9d0baeb86535d
Eshitajha19 <jha.eshita2023@gmail.com>
2025-08-03 10:40:18 +0530

    Update login.js

--- login.js
+++ login.js
@@ -1,28 +1,3 @@
-// Firebase v11.9.0 imports
-import { initializeApp } from "https://www.gstatic.com/firebasejs/11.9.0/firebase-app.js";
-import {
-  getAuth,
-  setPersistence,
-  browserLocalPersistence,
-  browserSessionPersistence,
-  signInWithEmailAndPassword,
-  signInWithPopup,
-  sendPasswordResetEmail,
-  signOut,
-  GoogleAuthProvider,
-  onAuthStateChanged
-} from "https://www.gstatic.com/firebasejs/11.9.0/firebase-auth.js";
-
-// 🔐 Firebase config
-const firebaseConfig = {
-   apiKey: "AIzaSyAPXYOQk3LPxayuymhac79P4QsX13xvMZA",
-    authDomain: "edu-navigator-cc4a0.firebaseapp.com",
-    projectId: "edu-navigator-cc4a0",
-    storageBucket: "edu-navigator-cc4a0.appspot.com",
-    messagingSenderId: "33607968332",
-    appId: "1:33607968332:web:59cbf34a0aae68375736e9"
-};
-
 // ✅ Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
@@ -114,4 +89,5 @@ onAuthStateChanged(auth, (user) => {
     console.log("User signed in:", user.email);
   }
 });
+
 
